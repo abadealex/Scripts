0cafa9bf7e8fb5ed6cff306a75b285d6897a7934
amooti365 <alexbarugahara@gmail.com>
2025-06-15 18:52:53 +0300

    Added StudentUploadForm to fix deployment crash

--- cograder_clone/app/forms.py
+++ cograder_clone/app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
-from wtforms.validators import InputRequired, Email, Length, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField, FileField
+from wtforms.validators import InputRequired, Email, Length, EqualTo, DataRequired
 
+# ---------- Registration Form ----------
 class RegisterForm(FlaskForm):
     email = StringField(
         "Email",
@@ -31,6 +32,7 @@ class RegisterForm(FlaskForm):
     )
     submit = SubmitField("Register")
 
+# ---------- Login Form ----------
 class LoginForm(FlaskForm):
     email = StringField(
         "Email",
@@ -45,3 +47,9 @@ class LoginForm(FlaskForm):
     )
     remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
+
+# ---------- Student File Upload Form ----------
+class StudentUploadForm(FlaskForm):
+    guide_id = SelectField("Choose Guide", coerce=int, validators=[DataRequired()])
+    file = FileField("Upload Answer File", validators=[DataRequired()])
+    submit = SubmitField("Submit")
